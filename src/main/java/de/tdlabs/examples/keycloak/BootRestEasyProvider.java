d4db0061a9b195aab55508fb0a9a94cdb8dc08b1
Mike Meessen <netmikey@gmail.com>
2019-11-23 14:12:11 +0100

    bring back the ServletContext augmentation logic

--- src/main/java/de/tdlabs/examples/keycloak/BootRestEasyProvider.java
+++ src/main/java/de/tdlabs/examples/keycloak/BootRestEasyProvider.java
@@ -3,18 +3,34 @@
  */
 package de.tdlabs.examples.keycloak;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+
+import javax.servlet.ServletContext;
+
 import org.jboss.resteasy.core.Dispatcher;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.keycloak.common.util.ResteasyProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author mike
  */
 public class BootRestEasyProvider implements ResteasyProvider {
 
+	private static final Logger LOG = LoggerFactory.getLogger(BootRestEasyProvider.class);
+	
+	static KeycloakServerProperties keycloakServerProperties;
+
 	@Override
 	public <R> R getContextData(Class<R> type) {
-		return ResteasyProviderFactory.getContextData(type);
+		if (ServletContext.class.equals(type)) {
+			return (R) augmentToRedirectContextPath((ServletContext) ResteasyProviderFactory.getContextData(type));
+		} else {
+			return ResteasyProviderFactory.getContextData(type);
+		}
 	}
 
 	@Override
@@ -33,4 +49,26 @@ public class BootRestEasyProvider implements ResteasyProvider {
 		ResteasyProviderFactory.clearContextData();
 	}
 
+    private static ServletContext augmentToRedirectContextPath(ServletContext servletContext) {
+
+        ClassLoader classLoader = servletContext.getClassLoader();
+        Class[] interfaces = {ServletContext.class};
+
+        InvocationHandler invocationHandler = (proxy, method, args) -> {
+
+            if ("getContextPath".equals(method.getName())) {
+                return keycloakServerProperties.getContextPath();
+            }
+
+            if ("getInitParameter".equals(method.getName()) && args.length == 1 && "keycloak.embedded".equals(args[0])) {
+                return "true";
+            }
+
+            LOG.info("Invoke on ServletContext: method=[{}] args=[{}]", method.getName(), Arrays.toString(args));
+
+            return method.invoke(servletContext, args);
+        };
+
+        return ServletContext.class.cast(Proxy.newProxyInstance(classLoader, interfaces, invocationHandler));
+    }
 }
